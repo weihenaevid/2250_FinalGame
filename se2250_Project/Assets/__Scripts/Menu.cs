feaa46008b6e3e58a661f1c97291c486e4b3b391
mariam-alzu3 <90797083+mariam-alzu3@users.noreply.github.com>
2022-03-31 11:41:28 -0400

    more menu additions + cleaned up some stuff

--- se2250_Project/Assets/__Scripts/Menu.cs
+++ se2250_Project/Assets/__Scripts/Menu.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class Menu : MonoBehaviour
 {
     // 
-    public Text levelText, xpText;
+    public Text levelText, xpText, hitpointText;
 
     //private int currentCharacterSelection = 0;
     public Image characterSelectionSprite, weaponSprite; 
@@ -26,6 +26,14 @@ public class Menu : MonoBehaviour
     public void UpdateMenu()
     {
         //weaponSprite.sprite = GameManager.instance.weaponSprite[0];
+
+        levelText.text = "Not Implemented";
+        hitpointText.text = GameManager.instance.player.hitpoint.ToString();
+
+        //exp bar
+        xpText.text = "Not imp";
+        xpBar.localScale = new Vector3 (0.5f, 0, 0);
+
     }
     
 }
