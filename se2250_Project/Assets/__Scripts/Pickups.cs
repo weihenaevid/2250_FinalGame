2c5935276052c545fe5665b5836a5084ba9811c5
Dij1022 <90654140+Divija1967@users.noreply.github.com>
2022-04-02 22:33:51 -0400

    images pop up when collect collectibles in main level

--- se2250_Project/Assets/__Scripts/Pickups.cs
+++ se2250_Project/Assets/__Scripts/Pickups.cs
@@ -10,7 +10,7 @@ public class Pickups : MonoBehaviour
     public Image imageKey1, imageKey2, imageKey3, imageGPotion, imageRPotion, imageChest;//the images in the inventory bar
     public int powerupsCollected = 0;
     //private Scene currentScene;
-
+    
     // Start is called before the first frame update
     void Start()
     {   
@@ -20,7 +20,7 @@ public class Pickups : MonoBehaviour
         imageGPotion.enabled = false;
         imageRPotion.enabled = false;
         imageChest.enabled = false;
-        
+
         if(SceneManager.GetActiveScene().name == "BigBossLevel")
         {
             imageKey1.enabled = true;
@@ -38,38 +38,37 @@ public class Pickups : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        //if(other.CompareTag("Fighter"))
-        if(other.gameObject.tag == "Fighter")
+        if(other.CompareTag("Fighter"))
         {
             Debug.Log("pickup");
             Debug.Log(other.name);
 
-            // if(gameObject.name == "key1")//check if this gameObject is key1
-            // {
-            //     imageKey1.enabled = true;
-            // }
-            // if(gameObject.name == "key2")//check if this gameObject is key2
-            // {
-            //     imageKey2.enabled = true;
-            // }
-            // if(gameObject.name == "key3")//check if this gameObject is key3
-            // {
-            //     imageKey3.enabled = true;
-            // }
-            // if(gameObject.name == "gPotion")//check if this gameObject is gPotion
-            // {
-            //     imageGPotion.enabled = true;
-            // }
-            // if(gameObject.name == "rPotion")//check if this gameObject is rPotion
-            // {
-            //     imageRPotion.enabled = true;
-            // }
-            // if(gameObject.name == "chest")//check if this gameObject is chest
-            // {
-            //     imageChest.enabled = true;
-            // }
+            if(gameObject.name == "key_1" )//check if this gameObject is key1
+            {
+                imageKey1.enabled = true;
+            }
+            if(gameObject.name == "key_2")//check if this gameObject is key2
+            {
+                imageKey2.enabled = true;
+            }
+            if(gameObject.name == "key_3")//check if this gameObject is key3
+            {
+                imageKey3.enabled = true;
+            }
+            if(gameObject.name == "greenPotion")//check if this gameObject is gPotion
+            {
+                imageGPotion.enabled = true;
+            }
+            if(gameObject.name == "redPotion")//check if this gameObject is rPotion
+            {
+                imageRPotion.enabled = true;
+            }
+            if(gameObject.name == "chest")//check if this gameObject is chest
+            {
+                imageChest.enabled = true;
+            }
             
-            // powerupsCollected += 1;
+            powerupsCollected += 1;
             Destroy(gameObject);
             //wait 4 seconds
             
